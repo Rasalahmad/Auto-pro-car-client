e7244b31358e84c5d961148716fd60c7d07dc9cb
Rasalahmad <rasalahmadsit@gmail.com>
2021-11-11 00:51:59 +0600

    Show reviews in ui

--- src/pages/Home/Review/Review.js
+++ src/pages/Home/Review/Review.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/system';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
 
 const Review = () => {
+    const [collection, setCollection] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/review')
+            .then(res => res.json())
+            .then(data => {
+                setCollection(data);
+            })
+    }, [])
     return (
-        <div>
-            <h2>Review</h2>
-        </div>
+        <Box>
+            <Typography variant="h4" sx={{ my: '50px' }} component="div">
+                Clients Review
+            </Typography>
+            <Grid container spacing={2}>
+                {
+                    collection.map(pd => <Grid
+                        item xs={12} md={4}
+                        key={pd._id
+                        }
+                    >
+                        {/* <img src={pd.img} width="100%" alt=""/>
+                        {pd.name} */}
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardMedia
+                                component="img"
+                                alt="green iguana"
+                                height="140"
+                                image={pd?.img}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h6" component="div">
+                                    {pd.name}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {pd.description}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {pd.rating}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>)
+                }
+            </Grid>
+        </Box>
     );
 };
 
