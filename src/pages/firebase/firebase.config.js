e6f0beb73df4a5ab017d41c25c49fc9c272a9798
Rasalahmad <rasalahmadsit@gmail.com>
2021-11-11 13:30:21 +0600

    add firebase auth sestem

---
+++ src/pages/firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCOsodcAISP37lAbpqvdC3yJx_C1pdEYxE",
+    authDomain: "auto-pro-luxurious-car.firebaseapp.com",
+    projectId: "auto-pro-luxurious-car",
+    storageBucket: "auto-pro-luxurious-car.appspot.com",
+    messagingSenderId: "846177774983",
+    appId: "1:846177774983:web:a8f11aec2b7a8da53f3722"
+};
+export default firebaseConfig;
