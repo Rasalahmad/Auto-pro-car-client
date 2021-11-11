ea256ddd51c7eb2825ce8030847a5d0520ce0908
Rasalahmad <rasalahmadsit@gmail.com>
2021-11-11 12:01:24 +0600

    insert details page

--- src/pages/Home/CarDetails/CarDetails.js
+++ src/pages/Home/CarDetails/CarDetails.js
@@ -1,26 +1,90 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { Box } from '@mui/system';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 const CarDetails = () => {
-    const {carId} = useParams();
+    const { carId } = useParams();
     const [details, setDetails] = useState([]);
-    useEffect( () => {
+    useEffect(() => {
         fetch(`http://localhost:5000/carDetails/${carId}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            setDetails(data)
-        })
-    }, [])
+            .then(res => res.json())
+            .then(data => {
+                setDetails(data)
+            })
+    }, [carId])
     return (
-        <div>
-            <h2>Car Detals of {carId}</h2>
-            {/* {
-                details.map(dt => <div>
-                    <h2>{dt?.name}</h2>
-                </div>)
-            } */}
-        </div>
+        <Box>
+            <Typography gutterBottom variant="h4" component="div" sx={{ my: "100px", fontWeight: 'bold' }}>
+                                <span style={{color: 'blue'}}>{details.name}'s</span> SPECIFICATION
+                            </Typography>
+            <Grid container spacing={2}>
+                <Grid item xs={8} md={6}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            alt="green iguana"
+                            height="300"
+                            image={details.img}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {details?.name}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Price ${details.price}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Place Order</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={4} md={6}>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Specification</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Brand Name</th>
+                                <td>{details?.name}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Body Style</th>
+                                <td>{details?.bodyStyle}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Engine</th>
+                                <td>{details?.engine}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Color</th>
+                                <td>{details?.color}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Drive Type</th>
+                                <td>{details?.driveType}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Transmission</th>
+                                <td>{details?.transmission}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </Grid>
+            </Grid>
+        </Box>
     );
 };
 
